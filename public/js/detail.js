13e0e6cf722749fc8a491cb71bc0c11d1d1c6be6
Kelly Chen <kellychen841106@gmail.com>
2022-07-17 09:35:07 +0800

    [Fix] Fix comment time in detail page

--- public/js/detail.js
+++ public/js/detail.js
@@ -46,9 +46,7 @@ function computeDueTime(startDate){
       .format("YYYY-MM-DD");
     let dueDate_ISO = covertToISOtime(dueDate);
     refund_duedate = moment(dueDate_ISO).format("YYYY-MM-DD HH:mm:ss");
-    console.log(moment(dueDate_ISO).format("YYYY-MM-DD HH:mm:ss"))
-    console.log(moment(dueDate_ISO).format("YYYY-MM-DD HH:MM:SS"))
-    console.log(moment(dueDate_ISO).format("YYYY-MM-DD HH:mm:ss"))
+    
     refund_duedate_timestamp = moment(dueDate_ISO).valueOf();
     return refund_duedate
 
@@ -219,7 +217,7 @@ async function rederData() {
       let comment_date = parseInt(review.created_at);
       comment_date = moment(comment_date)
         .tz("Asia/Taipei")
-        .format("YYYY-MM-DD HH:MM:SS");
+        .format("YYYY-MM-DD HH:mm:ss");
       clone.find("small").text(comment_date);
       clone.find("p").text(review.comment);
       clone.removeAttr("style");
